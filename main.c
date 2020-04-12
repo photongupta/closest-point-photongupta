805bddbfa641915caaeef560a63e513a950f0426
Rashmi Gupta <rashmi224141@gmail.com>
2020-04-12 20:53:36 +0530

    Modefied variable name

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void)
   Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
   int food_points_length = 5;
   Point organism_location;
-  Point closest_food;
+  Point closest_food_location;
 
   printf("Enter your position:");
   NEW_LINE;
@@ -16,9 +16,9 @@ int main(void)
   printf("y:");
   scanf("%d", &organism_location.y);
 
-  get_closest_food(food_points, food_points_length, organism_location, &closest_food);
+  get_closest_food(food_points, food_points_length, organism_location, &closest_food_location);
 
-  printf("Closest food target is x:%d y:%d", closest_food.x, closest_food.y);
+  printf("Closest food target is x:%d y:%d", closest_food_location.x, closest_food_location.y);
 
   return 0;
 }
