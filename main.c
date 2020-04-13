487f06b2a03efb53c80c04b53bde388b63f187b2
Rashmi Gupta <rashmi224141@gmail.com>
2020-04-13 14:36:54 +0530

    Removed unnecessary variable food_points_length

--- main.c
+++ main.c
@@ -5,7 +5,6 @@ int main(void)
 
 {
   Point food_points[POINTS_LENGTH] = FOOD_POINTS;
-  int food_points_length = POINTS_LENGTH;
   Point organism_location;
   Point closest_food_location;
 
@@ -16,7 +15,7 @@ int main(void)
   printf("y:");
   scanf("%d", &organism_location.y);
 
-  get_closest_food(food_points, food_points_length, organism_location, &closest_food_location);
+  get_closest_food(food_points, POINTS_LENGTH, organism_location, &closest_food_location);
 
   printf("Closest food target is x:%d y:%d", closest_food_location.x, closest_food_location.y);
 
